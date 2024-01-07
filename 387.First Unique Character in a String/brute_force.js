68133b7bfe35fd702a91aeb4406c16b9a19e7464
Rajat Dua <rajatdua1994@gmail.com>
2024-01-07 21:29:24 +0530

    brute force approach done

---
+++ 387.First Unique Character in a String/brute_force.js
@@ -0,0 +1,14 @@
+var firstUniqChar = function (s) {
+  for (let i = 0; i < s.length; i++) {
+    let isUnique = true;
+    for (let j = 0; j < s.length; j++) {
+      if (i != j && s[i] === s[j]) {
+        isUnique = false;
+        break;
+      }
+    }
+    if (isUnique) return i;
+  }
+
+  return -1;
+};
